4c176904d5d053675bc4e1f6c1df47f25ad60b43
xzackli <xzackli@gmail.com>
2021-04-04 14:45:48 -0400

    slow specific function in nested circumstance

--- test/runtests.jl
+++ test/runtests.jl
@@ -31,11 +31,21 @@ end
 # end
 
 
-# @testset "slow a specific function" begin
-#     @slowdef f(x; y=0) = return y
-#     f(x; y=0) = 0
-#     @test @slow(f, f(1; y=1)) == 1
-# end
+@testset "slow a specific function" begin
+    # fake naive implementation
+    @slowdef function f(x)
+        return 0
+    end
+    f(x) = x
+
+    @slowdef function g(x)
+        return f(x) * 2
+    end
+    g(x) = f(x) * 3
+    @test @slow(f, g(1)) == 0
+    @test @slow(g, g(1)) == 2
+    @test @slow(g(1)) == 0
+end
 
 
 @testset "nesting" begin
